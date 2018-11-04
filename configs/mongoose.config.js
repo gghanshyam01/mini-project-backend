06483248b7d3adb94417816b8968c62be07e8596
Ghanshyam Gupta <gghanshyam01@gmail.com>
2018-11-04 22:19:29 +0530

    Added keys to heroku

---
+++ configs/mongoose.config.js
@@ -0,0 +1,8 @@
+mongooseOptions = {
+    user: 'ghanshyam',
+    pass: 'Ghan$hyam@01',
+    useNewUrlParser: true
+};
+const URL = 'mongodb://ds223253.mlab.com:23253/motorhood-crm-dev';
+
+module.exports = {mongooseOptions, URL};
---
+++ email/credentials.js
@@ -0,0 +1,6 @@
+gmail = {
+    username: 'gghanshyam01@gmail.com',
+    password: '1amG#@n$hyam@01'
+};
+app = '';
+module.exports = gmail;
