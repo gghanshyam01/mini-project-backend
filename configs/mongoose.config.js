f554359a3f41f2da00cfc6a3b274172c667e28d1
Ghanshyam Gupta <gghanshyam01@gmail.com>
2018-11-23 23:43:02 +0530

    Many Changes

--- configs/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-mongooseOptions = {
-    user: 'ghanshyam',
-    pass: 'Ghan$hyam@01',
-    useNewUrlParser: true
-};
-const URL = 'mongodb://ds223253.mlab.com:23253/motorhood-crm-dev';
-
-module.exports = {mongooseOptions, URL};
--- email/credentials.js
+++
@@ -1,6 +0,0 @@
-gmail = {
-  username: 'gghanshyam01@gmail.com',
-  password: 'h@lfBl0odPr1nc3G#@n$hyam1'
-};
-app = '';
-module.exports = gmail;
